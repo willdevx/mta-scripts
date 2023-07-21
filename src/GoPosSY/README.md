c5abcdb7b802af37863eb63b496bc729b04d445e
willdevx <williamcarnneiro@gmail.com>
2023-07-21 13:10:06 -0300

    GoPosSY resource added

---
+++ src/GoPosSY/README.md
@@ -0,0 +1,7 @@
+# Sistema de Setar Posição (GoPosSY)
+
+### Comandos:
++ /setar [x] [y] [z] - Teleporta para posição
+
+### Permissão necessária:
+function.kickPlayer
---
+++ src/GoPosSY/server.lua
@@ -0,0 +1,25 @@
+﻿--[[-----------------------------------------
+-- This resource was created by Namykazesz.
+-- (c) 2017 BnF Server. All rights reserved.
+--]]-----------------------------------------
+
+-- initial properties
+local resourceCreationDate = "2017"
+local resourceOfficialName = "GoPosSY"
+local resourceDescription = "Sistema de Setar Posições"
+
+-- this will set the player position
+function setPosition(theClient, theCommand, x, y, z)
+	if(hasObjectPermissionTo(theClient, "function.kickPlayer")) then
+		if(x and y and z) then
+			setElementPosition(theClient, x, y, z)
+		else
+			theClient:outputChat("* Por favor, informe o x, y e z.",255,0,0)
+		end
+	else
+		theClient:outputChat("* Você não tem permissão para fazer isso.")
+	end
+end
+
+-- command
+addCommandHandler("setar", setPosition)
