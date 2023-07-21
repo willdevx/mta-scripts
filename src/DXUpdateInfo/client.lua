ea05dab6ddab632dd4662aab3d9010bca7ece6d6
willdevx <williamcarnneiro@gmail.com>
2023-07-21 13:07:20 -0300

    DXUpdateInfo resource added

---
+++ src/DXUpdateInfo/client.lua
@@ -0,0 +1,16 @@
+--[[-----------------------------------------
+-- This resource was created by Namykazesz.
+-- (c) 2018 BnF Server. All rights reserverd.
+--]]-----------------------------------------
+
+-- initial properties
+screenx, screeny = guiGetScreenSize()
+
+-- this will draw the update info message
+function drawUpdateInfo()
+	dxDrawText("[BnF v2.0] O server foi atualizado.", screenx-205, screeny-46, 0, 0, tocolor(0, 0, 0, 255), 1, "default-bold")
+	dxDrawText("[BnF v2.0] O server foi atualizado.", screenx-206, screeny-47, 0, 0, tocolor(255, 0, 0, 255), 1, "default-bold")
+end
+
+-- event
+addEventHandler("onClientRender", root, drawUpdateInfo)
