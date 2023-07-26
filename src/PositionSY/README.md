6b030feca7dfafcea6106cc715f5b472785d683a
willdevx <williamcarnneiro@gmail.com>
2023-07-26 15:57:55 -0300

    PositionSY resource added

---
+++ src/PositionSY/README.md
@@ -0,0 +1,6 @@
+# Sistema de Posição (PositionSY)
+
+Sistema para mostrar a posição atual no chat.
+
+### Comandos:
++ `/pos` - Comando para mostrar a posição atual x,y,z
---
+++ src/PositionSY/server.lua
@@ -0,0 +1,18 @@
+﻿--[[-----------------------------------------
+-- This resource was created by Namykazesz.
+-- (c) 2017 BnF Server. All rights reserved.
+--]]-----------------------------------------
+
+-- initial properties
+local resourceCreationDate = "2017"
+local resourceOfficialName = "Position"
+local resourceDescription = "Sistema para mostrar a posição"
+
+-- this will get the player position
+function getPosition(theClient)
+	x,y,z = getElementPosition(theClient)
+	theClient:outputChat("* Sua posição é (x,y,z): #FF9900"..x.."#FFFFFF, #FF9900"..y.."#FFFFFF, #FF9900"..z,255,255,255,true)
+end
+
+-- command
+addCommandHandler("pos", getPosition)
